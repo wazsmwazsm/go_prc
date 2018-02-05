9ca451a3ef8c9e6437d14f7bde87976649dd470d
MrQin <18748406022@163.com>
2018-02-05 23:30:56 +0800

    tour chapter 3 update

---
+++ tour/chapter3/struct-fields.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+    X int
+    Y int
+}
+
+func main() {
+    v := Vertex{1, 2}
+    v.X = 4
+
+    fmt.Println(v.X)
+
+    a := v
+    a.X = 5
+    fmt.Println(a)
+    fmt.Println(v)
+}
---
+++ tour/chapter3/struct-literals.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+    X, Y int
+}
+
+var (
+    v1 = Vertex{1, 2} // 类型为 Vertex
+    v2 = Vertex{X: 1} // Y:0 被省略
+    v3 = Vertex{}     // X:0 和 Y:0
+    p = &Vertex{1, 2} // 结构体指针
+)
+
+
+func main() {
+    fmt.Println(v1, p, v2, v3)
+}
