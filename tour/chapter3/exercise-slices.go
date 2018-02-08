50afbc62a6f174f98630b3b62456f57cda5754d5
MrQin <18748406022@163.com>
2018-02-08 23:01:48 +0800

    update tour chapter3

---
+++ tour/chapter3/exercise-slices.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "golang.org/x/tour/pic"
+    "math"
+)
+
+func Pic(dx, dy int) [][]oint8 {
+    var s [][]uint8
+
+    for i := 0; i < dy; i++ {
+        var tmp []uint8
+        for j := 0; j < dx; j++ {
+            pixel := float64(i) * math.Log(float64(j))
+            tmp = append(tmp, uint8(pixel))
+        }
+
+        s = append(s, tmp)
+    }
+
+    return s
+}
+
+func main() {
+    pic.Show(Pic)
+}
