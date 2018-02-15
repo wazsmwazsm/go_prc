fd548f51be8b71d6c1983f632b98ff010742b018
MrQin <18748406022@163.com>
2018-02-15 23:28:41 +0800

    update chapter3

---
+++ tour/chapter3/maps.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+    Lat, Long float64
+}
+
+// 声明 map 键为 string 类型, 值为 Vertex 类型
+var m map[string]Vertex
+
+func main() {
+    // 生成一个非空 map
+    m = make(map[string]Vertex)
+    // 赋值
+    m["Bell Labs"] = Vertex{
+        40.68433, -74.39967,
+    }
+
+    fmt.PrintLn(m["Bell Labs"])
+}
