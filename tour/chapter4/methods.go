f4501ecb6a81eca02e989b37c4e09360a06a51b2
MrQin <18748406022@163.com>
2018-02-24 22:06:04 +0800

    update tour chapter4 add methods

---
+++ tour/chapter4/methods.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type Vertex struct {
+    X, Y float64
+}
+
+// 定义结构体 Vertex 的方法
+func (v Vertex) Abs() float64 {
+    return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+func main() {
+    v := Vertex{3, 4}
+    fmt.Println(v.Abs())
+}
