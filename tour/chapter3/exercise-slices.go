4ff322a03e3603a211bc09d4a4579a38f40c7109
wazsmwazsm <18748406022@163.com>
2018-03-02 18:02:45 +0800

    update tour

--- tour/chapter3/exercise-slices.go
+++ tour/chapter3/exercise-slices.go
@@ -5,21 +5,39 @@ import (
     "math"
 )
 
+// func Pic(dx, dy int) [][]uint8 {
+//     var s [][]uint8
+//
+//     for i := 0; i < dy; i++ {
+//         var tmp []uint8
+//         for j := 0; j < dx; j++ {
+//             pixel := float64(i) * math.Log(float64(j))
+//             tmp = append(tmp, uint8(pixel))
+//         }
+//
+//         s = append(s, tmp)
+//     }
+//
+//     return s
+// }
+
 func Pic(dx, dy int) [][]uint8 {
-    var s [][]uint8
+    // 构造 slice
+    p := make([][]uint8, dy)
 
-    for i := 0; i < dy; i++ {
-        var tmp []uint8
-        for j := 0; j < dx; j++ {
-            pixel := float64(i) * math.Log(float64(j))
-            tmp = append(tmp, uint8(pixel))
+    for i := range p {
+        p[i] = make([]uint8, dx)
+    }
+    // 填充数据
+    for y, row := range p {
+        for x := range row {
+            row[x] = uint8(x * y)
         }
-
-        s = append(s, tmp)
     }
 
-    return s
+    return p
 }
+
 
 func main() {
     pic.Show(Pic)
