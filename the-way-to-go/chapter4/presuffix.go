a61f3af3b7cf0b93b80fd570671b7c03c9dcf6a9
wazsmwazsm <18748406022@163.com>
2018-03-27 19:54:11 +0800

    update the way to go chapter4

---
+++ the-way-to-go/chapter4/presuffix.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string = "This is an example of a string"
+	fmt.Printf("T/F? Does the string \"%s\" have prefix %s? ", str, "Th")
+	fmt.Printf("%t\n", strings.HasPrefix(str, "Th"))
+}
