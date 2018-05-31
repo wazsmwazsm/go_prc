8cf0d8179c205ac4786261db220d44fc55e39612
wazsmwazsm <18748406022@163.com>
2018-05-31 02:39:47 +0000

    update build-web-app-with-go

---
+++ build-web-application-with-golang/chapter4/submit_server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", sayhelloname)
+	http.HandleFunc("/submit", submit)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func sayhelloname(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // request.Form 是一个 url.Values 类型, 可以用 Set、Add 方法设置、添加内容
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello world!") 
+}
+
+func submit(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("submit.html")
+		log.Println(t.Execute(w, nil))
+	} else {
+		r.ParseForm()
+		// 
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
