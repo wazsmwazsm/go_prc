c4c00b50dc893e002df89dff9c5f9b1927450613
wazsmwazsm <942443360@qq.com>
2019-05-21 19:13:35 +0800

    update /code/go_prc/tour/chapter8

---
+++ the-way-to-go/chapter8/sort_map.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+var barVal = map[string]int{
+	"alpha": 34, "bravo": 56, "charlie": 23,
+	"delta": 87, "echo": 56, "foxtrot": 12,
+	"golf": 34, "hotel": 16, "indio": 87,
+	"juliet": 65, "kili": 43, "lima": 98,
+}
+
+func main() {
+	fmt.Println("unsorted:")
+	for k, v := range barVal {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
+
+}
+
+
+
