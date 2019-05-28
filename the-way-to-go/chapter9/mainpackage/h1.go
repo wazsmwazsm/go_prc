54cacef15172d6a466d0fe5160347103d483948e
wazsmwazsm <942443360@qq.com>
2019-05-28 17:28:12 +0800

    update the way to go chapter9

---
+++ the-way-to-go/chapter9/mainpackage/h1.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	show()
+	fmt.Println("main")
+}
---
+++ the-way-to-go/chapter9/mainpackage/h2.go
@@ -0,0 +1,8 @@
+// main 包可以有多个文件，但是只能有一个 main 函数
+package main
+
+import "fmt"
+
+func show() {
+	fmt.Println("show")
+}
