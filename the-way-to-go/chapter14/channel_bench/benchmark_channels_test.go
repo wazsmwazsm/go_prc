0680e64b398f7ccc2c95dca6d9a54a4a9ab2b1a1
wazsmwazsm <942443360@qq.com>
2019-08-26 22:51:34 +0800

    update the way to go chapter14

---
+++ the-way-to-go/chapter14/channel_bench/benchmark_channels_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkChannelSync(b *testing.B) {
+	ch := make(chan int)
+	go func() {
+		for i := 0; i < b.N; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+	for range ch {
+	}
+}
+
+func BenchmarkChannelBufferd(b *testing.B) {
+	ch := make(chan int, 128)
+	go func() {
+		for i := 0; i < b.N; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+	for range ch {
+	}
+}
